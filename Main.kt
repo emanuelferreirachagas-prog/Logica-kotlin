9194cebf5dc5bced760d105d0f60d108126694e9
Emanuel Chagas <emanuelferreirachagas@gmail.com>
2026-03-09 17:50:58 -0300

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var nome_do_carro = "Fusca"
+    println ("$nome_do_carro")
+    usuario()
+}
+fun usuario(){
+    var nomedousuario = readln()
+    println("Olá $nomedousuario")
+    idade()
+}
+fun idade(){
+    var nome = readln()
+    var idade = readln()
+
+    println("Olá $nome, que bom que possui $idade anos!")
+}
